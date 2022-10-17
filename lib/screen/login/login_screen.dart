8b437615931ffd45f2defe885442ab19f44fe301
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2022-10-17 21:03:23 +0200

    Saving login data in local secure storage

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../service/login_secure_storage_service.dart';
+
+const Color _signInButtonColor = Color.fromARGB(255, 213, 164, 25);
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -8,65 +12,120 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final serverController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future init() async {
+    final email = await LoginSecureStorage.getLoginEmail() ?? '';
+    final password = await LoginSecureStorage.getLoginPassword() ?? '';
+    final server = await LoginSecureStorage.getLoginServer() ?? '';
+
+    setState(() {
+      emailController.text = email;
+      passwordController.text = password;
+      serverController.text = server;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 4, 92, 179),
       body: Column(children: [
         Center(child: Image.asset('assets/tracardi_logo.png')),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(12)),
-              child: const Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: "Server"),
-                ),
-              )),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(12)),
-              child: const Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: "Email"),
-                ),
-              )),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(12)),
-              child: const Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: "Password"),
-                ),
-              )),
-        ),
+        serverField(),
+        const SizedBox(height: 10.0),
+        emailField(),
+        const SizedBox(height: 10.0),
+        passwordField(),
+        const SizedBox(height: 40.0),
+        signInButton()
       ]),
     );
   }
+
+  Widget serverField() => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: TextField(
+                controller: serverController,
+                decoration: const InputDecoration(
+                    border: InputBorder.none, hintText: "localhost:8686"),
+              ),
+            )),
+      );
+
+  Widget emailField() => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                    border: InputBorder.none, hintText: "Email"),
+              ),
+            )),
+      );
+
+  Widget passwordField() => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    border: InputBorder.none, hintText: "Password"),
+              ),
+            )),
+      );
+
+  Widget signInButton() => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () async {
+              LoginSecureStorage.setLoginData(emailController.text,
+                  passwordController.text, serverController.text);
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: _signInButtonColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12))),
+            child: const Center(
+                child: Text(
+              'Sign In',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            )),
+          ),
+        ),
+      );
 }
